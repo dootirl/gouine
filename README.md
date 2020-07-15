230c03da42ceabe0188e1e7338448f573e66a1fd
dootirl <dootirl@gmail.com>
2020-07-16 01:26:21 +0200

    git commit -m "$CURRCMD"

---
+++ README.md
@@ -0,0 +1,4 @@
+gouine
+go-lfed quine in go (115B)
+
+this readme is also a quine
---
+++ quine.go
@@ -0,0 +1,5 @@
+package main
+func main(){print(g,o,g,o)}
+var o,g="\x60",`package main
+func main(){print(g,o,g,o)}
+var o,g="\x60",`
